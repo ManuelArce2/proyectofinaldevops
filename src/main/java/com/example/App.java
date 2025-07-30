82fce2efa87cf509a4e113f6ca9f2db5ff856f20
Jose Manuel Arce <91504311+ManuelArce2@users.noreply.github.com>
2025-07-30 10:32:18 -0600

    update

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,4 +1,5 @@
 package com.example;
+import io.javalin.Javalin;
 
 /**
  * Hello world!
@@ -6,8 +7,11 @@ package com.example;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static Javalin createApp() {
+        return Javalin.create().get("/hello", ctx -> ctx.result("Hello World"));
     }
+
+    public static void main(String[] args) {
+        createApp().start(7000);
+}
 }
